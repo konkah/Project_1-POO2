358ba966361b26ce04709c75db53207d0d1feebd
Karlos Helton Braga <karlos.hbraga@gmail.com>
2020-11-01 00:46:51 -0300

    Add Controller Call to Start Program

--- digrafo/StartProgram.java
+++ digrafo/StartProgram.java
@@ -1,8 +1,15 @@
 package digrafo;
 
+import java.io.IOException;
+
 public class StartProgram {
 	public static void main(String args[]) {
-		
-		
+		Controller controller = new Controller();
+		try {
+			controller.main(args);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 }
