0733c252d9c1b2224ba553e61ba14738a5193ad3
Karlos Helton Braga <karlos.hbraga@gmail.com>
2020-11-02 14:53:52 -0300

    Make program to process arguments it receives

--- digraph/StartProgram.java
+++ digraph/StartProgram.java
@@ -5,17 +5,14 @@ import java.nio.file.Paths;
 
 public class StartProgram {
 	public static void main(String args[]) {
-		//for (int i = 0; i < args.length; i++) {
+		for (int i = 0; i < args.length; i++) {
 			try {
-				new Controller("documentation/Ex1_WantedAndWild.txt").CreateDigraphs();
-				new Controller("documentation/Ex2_AMascaraDaMorteRubra.txt").CreateDigraphs();
-				new Controller("documentation/Ex3_LuarDoSertao.txt").CreateDigraphs();
-				new Controller("documentation/Ex4_Parabens.txt").CreateDigraphs();
-				new Controller("documentation/Ex5_TresMotivosParaVacinarSeuPet.txt").CreateDigraphs();
+				Controller controller = new Controller(args[i]);
+				controller.CreateDigraphs();
 			} catch (IOException e) {
-				System.out.println("It was not possible to find file");
+				System.out.println("It was not possible to read the file");
 			}
-		//}
+		}
 	}
 
 	private final String currentDirectory = System.getProperty("user.dir");
